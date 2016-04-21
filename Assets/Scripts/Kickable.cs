8de30e05bafc31a0e4bf606da99512659e163339
Filippo <Filippo@131-193-76-154.evl.uic.edu>
2016-04-21 11:16:58 -0500

    first shaders

---
+++ Assets/Scripts/Kickable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kickable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("FootPadPresser")) {
+			float sp = other.gameObject.GetComponent<VelocityCalculator> ().speed;
+			Debug.Log (sp);
+		}
+	}
+}
---
+++ Assets/Scripts/VelocityCalculator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VelocityCalculator : MonoBehaviour {
+
+	public float speed = 0f;
+	public float speedTimeCalc = 1f;
+
+	private Vector3 lastPos;
+	// Use this for initialization
+	void Start () {
+		lastPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		speed = (Vector3.Distance (transform.position, lastPos)) / Time.fixedDeltaTime;
+		lastPos = transform.position;
+	}
+}
